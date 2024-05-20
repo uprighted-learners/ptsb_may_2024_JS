4911c31579f0c8eebe67fe6bc810f34a6b35de9e
u4ik <insighteuphoric@gmail.com>
2024-05-20 19:41:44 -0400

    commented intro

---
+++ data_types/index.js
@@ -0,0 +1,25 @@
+/* 
+    ? Javascript
+    * founded in 1996 Brandan Eich
+    * high level language
+    * client-sided language ( generally works in the browser )
+    * interpreted language ( not compiled )
+    * multi-paradigm - object oriented, functional, imperative
+    * prototype (usually accessed via '.' ex. .slice())
+    * weakly typed 
+    * ECMAScript -- organization setting the language specifications
+
+*/
+
+/* 
+    ? Comments
+    * blocks of code that are not being interpreted 
+    * as devs, it documents our code and makes it more maintainable
+    * denoted by // for single line (cmd/ctrl + /)
+    * denoted by /* for multiline  (opt/alt + shift + a)
+*/
+
+2+2;
+
+console.log(2+2); // Shows us the output in the console window
+
